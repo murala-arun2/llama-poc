f0df8c274f90d369de89f2442771f1f5cbc4ac91
arun murala <murala.arun2@gmail.com>
2025-03-14 07:34:37 +0530

    load data to chromadb

---
+++ docs-in-collection.py
@@ -0,0 +1,40 @@
+import chromadb
+from langchain_ollama import OllamaEmbeddings
+from chromadb.utils.embedding_functions import OllamaEmbeddingFunction
+from langchain_chroma import Chroma
+
+chroma_client = chromadb.HttpClient(host='localhost', port=8000)
+
+ef = OllamaEmbeddingFunction(
+    model_name="llama3.2",
+    url="http://localhost:11434/api/embeddings",
+)
+
+
+# chroma_client.delete_collection(name="spring-security")
+
+collection = chroma_client.get_or_create_collection(name="spring-security-2", embedding_function=ef)
+
+print('docs in collection:', collection.count())
+
+# print('doc :', collection.get(ids=['79e0914b-4b54-4f3b-90e9-6fdf1dc19028']))
+
+
+
+print('docs from query:', collection.query(
+    query_texts=["AuthenicationManager"],
+    # n_results=2,
+    # where={"source": "\\Users\\mural\\Downloads\\spring-security-main\\spring-security-main\\core\\src\\main\\java\\org\\springframework\\security\\access\\AccessDecisionManager.java"}
+    ))
+
+embed = OllamaEmbeddings(
+    model="llama3.2"
+)
+vector_store_from_client = Chroma(
+    client=chroma_client,
+    collection_name="spring-security-2",
+    embedding_function=embed,
+    create_collection_if_not_exists=False,
+)
+
+print("similarity search:", vector_store_from_client.similarity_search("AuthenicationManager", k=5))
---
+++ load-data-to-chromadb-langchain.py
@@ -0,0 +1,46 @@
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import TextLoader
+import chromadb
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_text_splitters import (
+    Language,
+    RecursiveCharacterTextSplitter,
+)
+
+embed = OllamaEmbeddings(
+    model="llama3.2"
+)
+
+# Initialize ChromaDB client
+chroma_client = chromadb.HttpClient(host='localhost', port=8000)
+
+# collection = chroma_client.get_or_create_collection(name="spring-security-2")
+
+# Define the path to the Java project source code folder
+java_project_path = "/Users/mural/Downloads/spring-security-main/spring-security-main/core/src/main/java"
+
+loader = DirectoryLoader(java_project_path, glob="**/*.java", loader_cls=TextLoader)
+docs = loader.load()
+print('total java files:', len(docs))
+print('docs[0]:', docs[0])
+
+splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.JAVA, chunk_size=50, chunk_overlap=0
+)
+all_splits = splitter.split_documents(docs)
+print('files after split:', len(all_splits))
+
+
+vector_store_from_client = Chroma(
+    client=chroma_client,
+    collection_name="spring-security-2",
+    embedding_function=embed,
+    create_collection_if_not_exists=True,
+)
+
+for doc in all_splits:
+    ids = vector_store_from_client.add_documents(documents=[doc])
+    print('document id:', ids)
+# ids = vector_store_from_client.add_documents(documents=all_splits)
+
